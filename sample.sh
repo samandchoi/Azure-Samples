378ce5408907d4b88601dda24f06bc25e0247052
samandchoi <48102007+samandchoi@users.noreply.github.com>
2021-01-22 17:23:46 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.262.b10-0.el7_8.x86_64/
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.275.b01-0.el7_9.x86_64
 . /data/SIEM/elasticsearch/startES
