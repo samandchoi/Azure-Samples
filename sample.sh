4d89074a88b0e02ce2c47b027fd0236211741072
samandchoi <48102007+samandchoi@users.noreply.github.com>
2021-01-25 14:36:21 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.275.b01-0.el7_9.x86_64
-su igloosec -c ". /data/SIEM/elasticsearch/startES"
+su igloosec
+/data/SIEM/elasticsearch/startES
